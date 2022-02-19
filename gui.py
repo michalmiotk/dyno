455c26b3429b24fba4e70d8ef31f7a725066487c
Michał Miotk <michalmiotk95@gmail.com>
2022-02-19 14:23:54 +0100

    split logic from gui

---
+++ gui.py
@@ -0,0 +1,110 @@
+from tkinter import Tk
+from tkinter import ttk
+from tkinter import filedialog as fd
+from figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+class Gui():
+    def __init__(self, connect_method, disconnect_method, save_figure_method, select_file_method):
+
+
+        self.connect_method = connect_method
+        self.disconnect_method = disconnect_method
+        self.save_figure_method = save_figure_method
+        self.select_file_method = select_file_method
+        self.figure = Figure()
+        self.root = Tk()
+        self.root.title('Hamownia')
+        self.frm = ttk.Frame(self.root, padding=10)
+        self.frm.grid()
+        ttk.Label(self.root, text="Wheel diameter in cm").grid(row=1, column=0)
+
+        self.wheel_diameter_entry = ttk.Entry(self.root)
+        self.wheel_diameter_entry.insert(0, '1')
+        self.wheel_diameter_entry.grid(row=1, column=1)
+
+        ttk.Label(self.root, text="Entire gear ratio").grid(row=2, column=0)
+
+        self.gear_ratio_entry = ttk.Entry(self.root)
+        self.gear_ratio_entry.insert(0, '1')
+        self.gear_ratio_entry.grid(row=2, column=1)
+
+        ttk.Label(self.root, text="Moto name").grid(row=3, column=0)
+
+        self.moto_name_entry = ttk.Entry(self.root)
+        self.moto_name_entry.grid(row=3, column=1)
+
+        self.communicates_label = ttk.Label(self.root)
+        self.communicates_label.grid(row=4, column=0)
+        
+        ttk.Button(self.frm, text="Open file", command=self.select_file_method).grid(row=0, column=0)
+        ttk.Button(self.frm, text="Save figure", command=self.save_figure_method).grid(row=0, column=1)
+
+        ttk.Button(text="Connect", command=self.connect_method).grid(row=6, column=0)
+        ttk.Button(text="Disonnect", command=self.disconnect_method).grid(row=6, column=1)
+        baud_label = ttk.Label(text="Baud")
+        baud_label.grid(row=7, column=0)
+
+        self.baud_entry = ttk.Entry(width=7)
+        self.baud_entry.grid(row=7, column=1)
+        self.baud_entry.insert(0, '9600')
+
+        port_label = ttk.Label(text="Port")
+        port_label.grid(row=8, column=0)
+        self.port_entry = ttk.Entry(width=7)
+        self.port_entry.grid(row=8, column=1)
+        self.port_entry.insert(0, '1')
+        self.draw_figure(self.figure.figure)
+
+    def draw_figure(self, figure):
+        canvas = FigureCanvasTkAgg(figure, self.root)
+        canvas.draw()
+        canvas.get_tk_widget().grid(row=5, columnspan=2)
+
+    def draw_internal_figure(self):
+        canvas = FigureCanvasTkAgg(self.figure.figure, self.root)
+        canvas.draw()
+        canvas.get_tk_widget().grid(row=5, columnspan=2)
+
+    def get_wheel_diameter(self):
+        return float(self.wheel_diameter_entry.get())
+
+    def get_gear_ratio(self):
+        return float(self.gear_ratio_entry.get())
+
+    def select_file(self):
+        pass
+
+    def save_figure(self):
+        pass
+        
+    def mainloop(self):
+        self.root.mainloop()
+
+    def get_port(self):
+        return self.port_entry.get()
+
+    def get_baud(self):
+        return self.baud_entry.get()
+
+    def get_moto_name(self):
+        return self.moto_name_entry.get()
+    
+    def set_communicates_label(self, text):
+        self.communicates_label.config(text=text)
+
+    def clear_communicates_label(self):
+        self.communicates_label.config(text = "")
+
+    def choose_filename(self):
+        filetypes = (
+            ('text files', '*'),
+            ('All files', '*.*')
+        )
+
+        filename = fd.askopenfilename(
+            title='Open a file',
+            initialdir='./',
+            filetypes=filetypes)
+        
+        return filename
--- main.py
+++ main.py
@@ -3,92 +3,32 @@ import queue
 import threading
 
 import serial
-from tkinter import Tk
-from tkinter import ttk
-from tkinter import filedialog as fd
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
 import pandas as pd
 
 from parse_file import get_df_from_file, is_valid_csv
 from data_from_df import add_to_df_with_wheel_torque, add_to_df_power_in_KM, add_to_df_engine_rot_speed
-from figure import Figure
-from df_from_uart import df_from_uart_row
 
+from df_from_uart import df_from_uart_row
+from gui import Gui
 
 
 class Program():
     def __init__(self):
-
+        self.gui = Gui(self.connect, self.disconnect, self.save_figure, self.select_file)
         self.serial_object = None
-
-        self.root = Tk()
-        self.root.title('Hamownia')
-        self.frm = ttk.Frame(self.root, padding=10)
-        self.frm.grid()
-
-        ttk.Label(self.root, text="Wheel diameter in cm").grid(row=1, column=0)
-
-        self.wheel_diameter_entry = ttk.Entry(self.root)
-        self.wheel_diameter_entry.insert(0, '1')
-        self.wheel_diameter_entry.grid(row=1, column=1)
-
-        ttk.Label(self.root, text="Entire gear ratio").grid(row=2, column=0)
-
-        self.gear_ratio_entry = ttk.Entry(self.root)
-        self.gear_ratio_entry.insert(0, '1')
-        self.gear_ratio_entry.grid(row=2, column=1)
-
-        ttk.Label(self.root, text="Moto name").grid(row=3, column=0)
-
-        self.moto_name_entry = ttk.Entry(self.root)
-        self.moto_name_entry.grid(row=3, column=1)
-
-        self.communicates_label = ttk.Label(self.root)
-        self.communicates_label.grid(row=4, column=0)
-
-        self.figure = Figure()
+                
         self.uart_process_queue = queue.Queue()
 
         self.update_gui_enabled = False
         self.uart_df = pd.DataFrame()
         self.serial_object = None
+        self.gui.mainloop()  
 
-        self.draw_figure(self.figure.figure)
-
-        ttk.Button(self.frm, text="Open file", command=self.select_file).grid(row=0, column=0)
-        ttk.Button(self.frm, text="Save figure", command=self.save_figure).grid(row=0, column=1)
-
-        ttk.Button(text="Connect", command=self.connect).grid(row=6, column=0)
-        ttk.Button(text="Disonnect", command=self.disconnect).grid(row=6, column=1)
-        baud_label = ttk.Label(text="Baud")
-        baud_label.grid(row=7, column=0)
-
-        self.baud_entry = ttk.Entry(width=7)
-        self.baud_entry.grid(row=7, column=1)
-        self.baud_entry.insert(0, '9600')
-
-        port_label = ttk.Label(text="Port")
-        port_label.grid(row=8, column=0)
-        self.port_entry = ttk.Entry(width=7)
-        self.port_entry.grid(row=8, column=1)
-        self.port_entry.insert(0, '1')
-        
-        self.root.mainloop()
-
-    def draw_figure(self, figure):
-        canvas = FigureCanvasTkAgg(figure, self.root)
-        canvas.draw()
-        canvas.get_tk_widget().grid(row=5, columnspan=2)
-
-    def get_wheel_diameter(self):
-        return float(self.wheel_diameter_entry.get())
-
-    def get_gear_ratio(self):
-        return float(self.gear_ratio_entry.get())
 
     def is_valid_gear_ratio(self):
         try:
-            self.get_gear_ratio()
+            self.gui.get_gear_ratio()
         except ValueError as e:
             print(e)
             print("not valid gear_ratio")
@@ -97,9 +37,10 @@ class Program():
     
         return False
 
+
     def is_valid_wheel_diameter(self):
         try:
-            self.get_wheel_diameter()
+            self.gui.get_wheel_diameter()
         except ValueError as e:
             print(e)
             print("not valid wheel_diameter")
@@ -107,59 +48,78 @@ class Program():
             return True
         
         return False
-
+    
 
     def select_file(self):
         
         if not self.is_valid_wheel_diameter():
-            self.communicates_label.config(text = "bad wheel diameter format")
+            self.gui.set_communicates_label("bad wheel diameter format")
             return
 
         if not self.is_valid_gear_ratio():
-            self.communicates_label.config(text = "bad gear ratio format")
+            self.gui.set_communicates_label("bad gear ratio format")
             return
     
-        filetypes = (
-            ('text files', '*'),
-            ('All files', '*.*')
-        )
-
-        filename = fd.askopenfilename(
-            title='Open a file',
-            initialdir='./',
-            filetypes=filetypes)
+        filename = self.gui.choose_filename()
 
         if filename == '':
-            self.communicates_label.config(text = "No choosen file")    
+            self.gui.set_communicates_label("No choosen file")    
             return
         
         if not is_valid_csv(filename):
-            self.communicates_label.config(text = "No valid csv")    
+            self.gui.set_communicates_label("No valid csv")    
             return
 
-        self.communicates_label.config(text = "")  
+        self.gui.clear_communicates_label()
+
         df = get_df_from_file(filename)
-        add_to_df_with_wheel_torque(df, wheel_diameter_in_cm=self.get_wheel_diameter())
+        add_to_df_with_wheel_torque(df, wheel_diameter_in_cm=self.gui.get_wheel_diameter())
         add_to_df_power_in_KM(df)
-        add_to_df_engine_rot_speed(df, self.get_wheel_diameter(), self.get_gear_ratio())
-        self.figure.update_figure(df)
-        self.draw_figure(self.figure.figure)
+        add_to_df_engine_rot_speed(df, self.gui.get_wheel_diameter(), self.gui.get_gear_ratio())
+        self.gui.figure.update_figure(df)
+        self.gui.draw_figure(self.gui.figure.figure)
 
 
     def save_figure(self):
-        moto_name = self.moto_name_entry.get()
+        moto_name = self.gui.get_moto_name()
         if moto_name == '':
-            self.communicates_label.config(text = "Empty moto name")    
+            self.gui.set_communicates_label("Empty moto name")    
             return
     
         now_time = str(datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S"))
-        if self.figure.is_figure_empty():
-            self.communicates_label.config(text = "No figure")
+        if self.gui.figure.is_figure_empty():
+            self.gui.set_communicates_label("No figure")
             return
 
-        self.communicates_label.config(text = "") 
+        self.gui.clear_communicates_label()
     
-        self.figure.save_figure(moto_name + '_' + now_time + '.png')
+        self.gui.figure.save_figure(moto_name + '_' + now_time + '.png')
+
+    
+    def update_gui(self, uart_process_q):
+        while (self.update_gui_enabled):
+            print("update")
+            try:
+                print("probouje zdobyc dane")
+                filter_data =uart_process_q.get()
+                print("plottime", filter_data)
+                new_df = df_from_uart_row(filter_data)
+                self.uart_df = pd.concat([self.uart_df, new_df])
+                add_to_df_with_wheel_torque(self.uart_df, wheel_diameter_in_cm=self.gui.get_wheel_diameter())
+                add_to_df_power_in_KM(self.uart_df)
+                add_to_df_engine_rot_speed(self.uart_df, self.gui.get_wheel_diameter(), self.gui.get_gear_ratio())
+                self.gui.figure.update_figure(self.uart_df)
+                self.gui.draw_figure(self.gui.figure.figure)
+            except queue.Empty as e:
+                print(e)
+        print("updating gui stopped")
+    
+
+    def start_update_gui_thread(self):
+        t2 = threading.Thread(target=self.update_gui, args=(self.uart_process_queue,))
+        t2.daemon = True
+        t2.start()
+    
 
     def get_data(self, queue, serial_obj):
         while serial_obj.isOpen():
@@ -178,33 +138,11 @@ class Program():
                 print("tajp error", e)
                 pass
 
-    def update_gui(self, uart_process_q):
-        while (self.update_gui_enabled):
-            print("update")
-            try:
-                print("probouje zdobyc dane")
-                filter_data =uart_process_q.get()
-                print("plottime", filter_data)
-                new_df = df_from_uart_row(filter_data)
-                self.uart_df = pd.concat([self.uart_df, new_df])
-                add_to_df_with_wheel_torque(self.uart_df, wheel_diameter_in_cm=self.get_wheel_diameter())
-                add_to_df_power_in_KM(self.uart_df)
-                add_to_df_engine_rot_speed(self.uart_df, self.get_wheel_diameter(), self.get_gear_ratio())
-                self.figure.update_figure(self.uart_df)
-                self.draw_figure(self.figure.figure)
-            except queue.Empty as e:
-                print(e)
-        print("updating gui stopped")
-
-    def start_update_gui_thread(self):
-        t2 = threading.Thread(target=self.update_gui, args=(self.uart_process_queue,))
-        t2.daemon = True
-        t2.start()
 
     def connect(self):
         self.uart_df = pd.DataFrame()
-        port = self.port_entry.get()
-        baud = self.baud_entry.get()
+        port = self.gui.get_port() 
+        baud = self.gui.get_baud() 
         try:
             self.serial_object = serial.Serial('COM' + str(port), baud)
 
@@ -219,11 +157,12 @@ class Program():
         self.update_gui_enabled = True
         self.start_update_gui_thread()
 
+
     def disconnect(self):
         if self.serial_object is not None:
             self.uart_df = pd.DataFrame()
             self.serial_object.close()
             self.update_gui_enabled = False
-
+    
 if __name__ == '__main__':
     program = Program()
